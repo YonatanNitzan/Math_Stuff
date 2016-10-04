84c9f589d8ab355471ca03f6a2f87e0955703a5a
compass <compass@compass-PC>
2016-10-04 17:18:01 +0300

    finished the circle equation

--- Math_Stuff/src/mathStuff/Average.java
+++ Math_Stuff/src/mathStuff/Average.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 
 public class Average extends Math_Ops{
 
-	static LinkedList<Double> inputs = new LinkedList<Double>();
-	private double input = 0;
-	private double sum = 0;
-	private double avr = 0;
+	static LinkedList<Float> inputs = new LinkedList<Float>();
+	private float input, sum;
+	private double avr;
 	
 	public Average()
 	{
@@ -21,7 +20,7 @@ public class Average extends Math_Ops{
         	try
         	{
 	        	System.out.print("Enter a number: ");
-				input = Main_Activity.scanner.nextDouble();
+				input = Main_Activity.scanner.nextFloat();
 		        inputs.add(input);
         	}
         	catch(Exception e)
@@ -37,8 +36,9 @@ public class Average extends Math_Ops{
         
         avr = sum / inputs.size();
         
-        System.out.println("The average is: " + avr);
-		System.out.print("Press \"ENTER\" to continue...");
+        System.out.println("\nThe average is: " + avr);
+        
+		System.out.print("\nPress \"ENTER\" to return to the main menu...");
 		try
 		{
 			System.in.read(new byte[2]);
--- Math_Stuff/src/mathStuff/Circle_Equation.java
+++ Math_Stuff/src/mathStuff/Circle_Equation.java
@@ -1,13 +1,83 @@
 package mathStuff;
 
+import java.io.IOException;
+
 public class Circle_Equation extends Math_Ops {
+	
+	private float midX, midY, radius;
+	private String xSection, ySection, radiusSection;
+	
 	public Circle_Equation()
 	{
 		main();
 	}
 	
-	/*protected void main()
+	protected void main()
 	{
+		try
+		{
+			System.out.print("Enter the x coordinates of the circle's middle point: ");
+			midX = Main_Activity.scanner.nextFloat();
+			System.out.print("Enter the y coordinates of the circle's middle point: ");
+			midY = Main_Activity.scanner.nextFloat();
+			System.out.print("Enter the circle's radius: ");
+			radius = Main_Activity.scanner.nextFloat();
+			
+			if(midX < 0)
+				xSection = String.format("(X + %.2f) + ", -midX);
+			else
+				xSection = String.format("(X - %.2f) + ", midX);
+			
+			if(midY < 0)
+				ySection = String.format("(Y + %.2f)", -midY);
+			else
+				ySection = String.format("(Y - %.2f)", midY);
+			
+			if(radius <= 0)
+			{
+				System.out.println("\nThat is not a posible radius");
+				System.out.println("Press \"ENTER\" to try again...");
+				try
+				{
+					System.in.read(new byte[2]);
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+				main();
+			}
+			else
+				radiusSection = String.format(" = %.2f", radius * 2);
+			
+			System.out.println("\nThe circle's equation is: " + xSection + ySection + radiusSection);
+			
+			System.out.print("Press \"ENTER\" to return to the main menu...");
+			try
+			{
+				System.in.read(new byte[2]);
+			}
+			catch (IOException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("\nThat is not a number");
+			System.out.print("Press \"ENTER\" to try again...\n");
+			try
+			{
+				System.in.read(new byte[2]);
+				Main_Activity.str = Main_Activity.scanner.next();
+			}
+			catch (IOException e1)
+			{
+				e1.printStackTrace();
+			}
+			main();
+		}
 		
-	}*/
+		
+	}
 }
